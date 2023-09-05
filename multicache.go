a44a74d6cea46c75461c3361cbcfd727b0a8c542
Doug Saylor <doug@saylorsolutions.com>
2023-09-04 19:55:26 -0500

    Adding MultiCache for caching multiple values, or trees of values

---
+++ multicache.go
@@ -0,0 +1,105 @@
+package cache
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+// MultiLoaderFunc is a lot like LoaderFunc, except that it accepts an input key.
+type MultiLoaderFunc[K comparable, V any] func(key K) (V, error)
+
+// MultiCache provides the ability to cache multiple values of type V by some comparable key K.
+// An example use-case would be caching database entities by primary key.
+type MultiCache[K comparable, V any] struct {
+	values map[K]*Value[V]
+	loader MultiLoaderFunc[K, V]
+	lock   sync.RWMutex
+	ttl    time.Duration
+}
+
+// NewMulti will create a new MultiCache with the given loader.
+// A MultiCache may be composed of other MultiCache in the case where a top level cache with heterogeneous values is needed.
+// If the loader is nil, then this function will panic.
+func NewMulti[K comparable, V any](loader MultiLoaderFunc[K, V]) *MultiCache[K, V] {
+	if loader == nil {
+		panic("nil loader")
+	}
+	return &MultiCache[K, V]{
+		values: map[K]*Value[V]{},
+		loader: loader,
+	}
+}
+
+// Preheat will load the values associated with each key in keys.
+// This will return the first error encountered and stop processing further keys.
+func (m *MultiCache[K, V]) Preheat(keys []K) error {
+	for _, key := range keys {
+		_, err := m.Get(key)
+		if err != nil {
+			return fmt.Errorf("error preheating cache with key '%v': %w", key, err)
+		}
+	}
+	return nil
+}
+
+// Get will return the value in the cache.Value associated with key K.
+// Any errors returned from [cache.Value.Get] will be returned from Get.
+func (m *MultiCache[K, V]) Get(key K) (V, error) {
+	m.lock.RLock()
+	c, ok := m.values[key]
+	if !ok {
+		m.lock.RUnlock()
+		m.populate(key)
+		return m.Get(key)
+	}
+	defer m.lock.RUnlock()
+	return c.Get()
+}
+
+func (m *MultiCache[K, V]) populate(key K) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+	if _, ok := m.values[key]; ok {
+		return
+	}
+
+	if m.loader == nil {
+		panic("nil loader")
+	}
+	c := New[V](func() (V, error) {
+		key := key
+		return m.loader(key)
+	})
+	if m.ttl > 0 {
+		c.SetTTL(m.ttl)
+	}
+	m.values[key] = c
+}
+
+// Invalidate will invalidate the cache.Value related to key K, if it exists.
+func (m *MultiCache[K, V]) Invalidate(key K) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+	c, ok := m.values[key]
+	if !ok {
+		return
+	}
+	c.Invalidate()
+}
+
+// SetTTLPolicy sets the time to live policy for all internal Value values after they are retrieved.
+// By default, a MultiCache value will not invalidate itself.
+// A TTL policy must be set prior to retrieval or preheating for any value to invalidate itself.
+//
+// Note that this policy takes precedence over any individual [Value]'s time to live value.
+//
+// This method will panic if ttl <= 0.
+func (m *MultiCache[K, V]) SetTTLPolicy(ttl time.Duration) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+	if ttl <= 0 {
+		panic("ttl <= 0")
+	}
+	m.ttl = ttl
+}
---
+++ multicache_test.go
@@ -0,0 +1,202 @@
+package cache
+
+import (
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestNewMulti(t *testing.T) {
+	var (
+		timesFetched int
+	)
+
+	type User struct {
+		Username string
+		Email    string
+	}
+	data := []*User{
+		{
+			Username: "bob",
+			Email:    "bob@gmail.com",
+		},
+		{
+			Username: "jen",
+			Email:    "jen@yahoo.com",
+		},
+		{
+			Username: "erin",
+			Email:    "erin@mail.com",
+		},
+	}
+	mc := NewMulti[string, *User](func(username string) (*User, error) {
+		timesFetched++
+		for _, user := range data {
+			if strings.ToLower(username) == strings.ToLower(user.Username) {
+				return user, nil
+			}
+		}
+		return nil, errors.New("not found")
+	})
+	assert.Equal(t, 0, timesFetched)
+
+	user, err := mc.Get("phil")
+	assert.Error(t, err)
+	assert.Equal(t, "not found", err.Error())
+	assert.Nil(t, user)
+	assert.Equal(t, 1, timesFetched)
+
+	user, err = mc.Get("erin")
+	assert.NoError(t, err)
+	assert.NotNil(t, user)
+	assert.Equal(t, 2, timesFetched)
+	assert.Equal(t, "erin@mail.com", user.Email)
+
+	_, _ = mc.Get("erin")
+	_, _ = mc.Get("erin")
+	_, _ = mc.Get("erin")
+	assert.Equal(t, 2, timesFetched)
+}
+
+func TestMultiCache_SetTTLPolicy(t *testing.T) {
+	const (
+		ttl = 50 * time.Millisecond
+	)
+	var (
+		timesFetched int
+	)
+
+	type User struct {
+		Username string
+		Email    string
+	}
+	data := []User{
+		{
+			Username: "bob",
+			Email:    "bob@gmail.com",
+		},
+		{
+			Username: "jen",
+			Email:    "jen@yahoo.com",
+		},
+		{
+			Username: "erin",
+			Email:    "erin@mail.com",
+		},
+	}
+	mc := NewMulti[string, User](func(username string) (User, error) {
+		timesFetched++
+		for _, user := range data {
+			if strings.ToLower(username) == strings.ToLower(user.Username) {
+				return user, nil
+			}
+		}
+		return User{}, errors.New("not found")
+	})
+	assert.Equal(t, 0, timesFetched)
+	mc.SetTTLPolicy(ttl)
+
+	_, _ = mc.Get("jen")
+	_, _ = mc.Get("jen")
+	_, _ = mc.Get("jen")
+	assert.Equal(t, 1, timesFetched)
+	time.Sleep(2 * ttl)
+
+	_, _ = mc.Get("jen")
+	assert.Equal(t, 2, timesFetched, "Jen's user cache should have been invalidated by time to live policy")
+}
+
+func TestMultiCache_Invalidate(t *testing.T) {
+	var (
+		timesFetched int
+	)
+
+	type User struct {
+		Username string
+		Email    string
+	}
+	data := []*User{
+		{
+			Username: "bob",
+			Email:    "bob@gmail.com",
+		},
+		{
+			Username: "jen",
+			Email:    "jen@yahoo.com",
+		},
+		{
+			Username: "erin",
+			Email:    "erin@mail.com",
+		},
+	}
+	mc := NewMulti[string, *User](func(username string) (*User, error) {
+		timesFetched++
+		for _, user := range data {
+			if strings.ToLower(username) == strings.ToLower(user.Username) {
+				return user, nil
+			}
+		}
+		return nil, errors.New("not found")
+	})
+	assert.Equal(t, 0, timesFetched)
+
+	_, _ = mc.Get("jen")
+	_, _ = mc.Get("jen")
+	_, _ = mc.Get("jen")
+	assert.Equal(t, 1, timesFetched, "only the first search for Jen should increment the fetch count")
+
+	_, _ = mc.Get("bob")
+	assert.Equal(t, 2, timesFetched, "searching for Bob should increment the fetch count, since it's a new record")
+	mc.Invalidate("bob")
+
+	_, _ = mc.Get("bob")
+	assert.Equal(t, 3, timesFetched, "searching for Bob again should increment the fetch count after invalidating this value")
+
+	_, _ = mc.Get("jen")
+	assert.Equal(t, 3, timesFetched, "Jen should still be cached")
+}
+
+func TestMultiCache_Preheat(t *testing.T) {
+	var (
+		timesFetched int
+		prefetchKeys = []string{"jen", "bob", "erin", "erin"}
+	)
+
+	type User struct {
+		Username string
+		Email    string
+	}
+	data := []*User{
+		{
+			Username: "bob",
+			Email:    "bob@gmail.com",
+		},
+		{
+			Username: "jen",
+			Email:    "jen@yahoo.com",
+		},
+		{
+			Username: "erin",
+			Email:    "erin@mail.com",
+		},
+	}
+	mc := NewMulti[string, *User](func(username string) (*User, error) {
+		timesFetched++
+		for _, user := range data {
+			if strings.ToLower(username) == strings.ToLower(user.Username) {
+				return user, nil
+			}
+		}
+		return nil, errors.New("not found")
+	})
+	assert.Equal(t, 0, timesFetched)
+	assert.NoError(t, mc.Preheat(prefetchKeys))
+	assert.Equal(t, len(prefetchKeys)-1, timesFetched, "A new fetch should have been done for each record, except for duplicates")
+
+	_, _ = mc.Get("jen")
+	_, _ = mc.Get("bob")
+	_, _ = mc.Get("erin")
+	assert.Equal(t, 3, timesFetched, "The records are already fetched and valid, so they should have been returned from cache")
+}
